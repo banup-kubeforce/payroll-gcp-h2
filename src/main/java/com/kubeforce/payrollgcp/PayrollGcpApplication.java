42ce542d19ee1eb82baf99126b294dcc3f7b8096
banu <banup@kubeforce.com>
2022-09-11 13:36:18 -0500

    Changed to FunctionalSpringApplication

--- src/main/java/com/kubeforce/payrollgcp/PayrollGcpApplication.java
+++ src/main/java/com/kubeforce/payrollgcp/PayrollGcpApplication.java
@@ -2,13 +2,14 @@ package com.kubeforce.payrollgcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PayrollGcpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PayrollGcpApplication.class, args);
+        FunctionalSpringApplication.run(PayrollGcpApplication.class, args);
     }
     @Bean
     public EmployeeFunction exampleFunction() {
